702ab0810a3571e9be8ab6c8d6956efd94fa5c56
Patrick OConnor <pjo2110@columbia.edu>
2017-05-22 07:40:06 -0400

    initalized

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,66 @@
-use std::net::{TcpListener, TcpStream};
-use std::iter::Iterator;
+// use std::net::{TcpListener, TcpStream};
+extern crate mio;
+use std::net::*;
+use mio::*;
+use mio::tcp::{TcpListener, TcpStream};
+use std::io::Write;
+use mio::tcp::Shutdown::Both;
+
+
+
+// use std::iter::Iterator;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_client(stream);
+    const SERVER: Token = Token(0);
+    // const CLIENT: Token = Token(1);
+
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    let server = TcpListener::bind(&addr).unwrap();
+
+    let poll = Poll::new().unwrap();
+
+    // Start listening for incoming connections
+    poll.register(&server, SERVER, Ready::readable(), PollOpt::edge())
+        .unwrap();
+    let mut events = Events::with_capacity(1024);
+    loop {
+        poll.poll(&mut events, None).unwrap();
+
+        for event in events.iter() {
+            match event.token() {
+                SERVER => {
+                    println!("Handling connection!");
+                    let (mut socket, addr) = server.accept().unwrap();
+                    let writeResult = socket.write(httpResponse().as_bytes());
+                    match writeResult {
+                        Ok(_) => {
+                            println!("Shutting down");
+
+                            socket.shutdown(Both);
+
+                        }
+                        Err(_) => {}
+                    }
+
+
+                    // println!("{:?}", socket);
+                }
+                _ => unreachable!(),
             }
-            Err(e) => println!("{:?}", e),
         }
     }
-    // let conns = listener.incoming().map(|stream| );
+
+
 }
 
 
-fn handle_client(stream: TcpStream) {
-    println!("{:?}", stream);
-}
+pub fn httpResponse() -> String {
 
-// // fn dump()
+    let message = format!(r#"HTTP-Version = HTTP/2.0
+Content-Type: text/html
+
+{}
+    "#,
+                          "WOW");
+    return String::from(message);
+}
